be092f300c7a6181d7c87ddfa19f384d2d2d0371
Juan David <juandavid918@gmail.com>
2024-10-14 12:18:24 -0500

    Microservicio de basket creado con CDK

--- lib/microservices.ts
+++ lib/microservices.ts
@@ -6,35 +6,67 @@ import { Runtime } from "aws-cdk-lib/aws-lambda";
 
 interface SwnMicroservicesProps {
     productTable: ITable;
+    basketTable: ITable;
 };
 
 export class SwnMicroservices extends Construct {
     public readonly productMicroservice: NodejsFunction;
+    public readonly basketMicroservice: NodejsFunction;
 
     constructor(scope: Construct, id: string, props: SwnMicroservicesProps) {
         super(scope, id);
+  
+        this.productMicroservice = this.createProductFunction(props.productTable);
+        this.basketMicroservice = this.createBasketFunction(props.basketTable);
+    }
 
-        const nodeJsFunctionProps: NodejsFunctionProps = {
-            bundling: {
-              externalModules: [
-                "aws-sdk"
-              ]
-            },
-            environment: {
-              PRIMARY_KEY: "id",
-              DYNAMODB_TABLE_NAME: props.productTable.tableName
-            },
-            runtime: Runtime.NODEJS_18_X
-          };
-      
-          // Product microservices Lambda function
-          const productFunction = new NodejsFunction(this, "productLambdaFunction", {
-            entry: join(__dirname, "/../src/product/index.js"),
-            ...nodeJsFunctionProps
-          });
-      
-          props.productTable.grantReadWriteData(productFunction);
+    createProductFunction(productTable: ITable): NodejsFunction {
+        const productFunctionProps: NodejsFunctionProps = {
+          bundling: {
+            externalModules: [
+              "aws-sdk"
+            ]
+          },
+          environment: {
+            PRIMARY_KEY: "id",
+            DYNAMODB_TABLE_NAME: productTable.tableName
+          },
+          runtime: Runtime.NODEJS_18_X
+        };
+    
+        // Product microservices Lambda function
+        const productFunction = new NodejsFunction(this, "productLambdaFunction", {
+          entry: join(__dirname, "/../src/product/index.js"),
+          ...productFunctionProps
+        });
+    
+        productTable.grantReadWriteData(productFunction);
+  
+        return productFunction;
+    }
 
-          this.productMicroservice = productFunction;
+    createBasketFunction(basketTable: ITable): NodejsFunction {
+        const basketFunctionProps: NodejsFunctionProps = {
+          bundling: {
+            externalModules: [
+              "aws-sdk"
+            ]
+          },
+          environment: {
+            PRIMARY_KEY: "userName",
+            DYNAMODB_TABLE_NAME: basketTable.tableName
+          },
+          runtime: Runtime.NODEJS_18_X
+        };
+    
+        // Product microservices Lambda function
+        const basketFunction = new NodejsFunction(this, "basketLambdaFunction", {
+          entry: join(__dirname, "/../src/basket/index.js"),
+          ...basketFunctionProps
+        });
+    
+        basketTable.grantReadWriteData(basketFunction);
+  
+        return basketFunction;
     }
 }
